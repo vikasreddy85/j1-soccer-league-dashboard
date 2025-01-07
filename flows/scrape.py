44a176c3a805d7a37a78ec334a328488808257f7
vikasreddy85 <vikasreddy2313@gmail.com>
2025-01-07 19:47:47 +0900

    Created prefect environment

--- flows/scrape.py
+++ flows/scrape.py
@@ -10,11 +10,4 @@ def web_scrape(name="world"):
     print_hello(name)
 
 if __name__ == "__main__":
-    flow.from_source(
-        "https://github.com/vikasreddy85/j1-soccer-league-dashboard.git",
-        entrypoint="flows/scrape.py:web_scrape",
-    ).deploy(
-        name="j1-league-container",
-        work_pool_name="my-docker-pool",
-        build=False
-    )
+    web_scrape()
